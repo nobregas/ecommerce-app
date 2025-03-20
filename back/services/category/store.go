fc75ddf61bbd78c8f95d8a43f62a4e7c4640d4e8
nobrega <gabrielnobregasantos2@gmail.com>
2025-03-20 15:42:52 -0300

    updated category structure

--- back/services/category/store.go
+++ back/services/category/store.go
@@ -1,6 +1,10 @@
 package category
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/nobregas/ecommerce-mobile-back/types"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +12,24 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
+}
+
+func (s *Store) GetCategories() (*[]types.Category, error) {
+	return nil, nil
+}
+
+func (s *Store) GetCategoryByID(categoryID int) (*types.Category, error) {
+	return nil, nil
+}
+
+func (s *Store) CreateCategory(payload types.CreateCategoryPayload) (*types.Category, error) {
+	return nil, nil
+}
+
+func (s *Store) UpdateCategory(categoryID int, payload types.UpdateCategoryPayload) (*types.Category, error) {
+	return nil, nil
+}
+
+func (s *Store) DeleteCategory(categoryID int) error {
+	return nil
 }
